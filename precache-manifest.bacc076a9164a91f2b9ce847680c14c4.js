23312d0645979fe9209ec829cc3880a83dfa52b5
chloe <cac499@cornell.edu>
2023-01-17 15:04:11 -0500

    Updates

---
+++ precache-manifest.bacc076a9164a91f2b9ce847680c14c4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "083f10ed7f91961a8c4f",
+    "url": "/website_v3.0/static/css/main.ea399c02.chunk.css"
+  },
+  {
+    "revision": "083f10ed7f91961a8c4f",
+    "url": "/website_v3.0/static/js/main.0f88f6e1.chunk.js"
+  },
+  {
+    "revision": "9b08fe55b1e780b82f24",
+    "url": "/website_v3.0/static/js/runtime~main.5726dc8e.js"
+  },
+  {
+    "revision": "ef2833db3ddefe8b1724",
+    "url": "/website_v3.0/static/js/2.8b1f3fa8.chunk.js"
+  },
+  {
+    "revision": "d73ddec02bdcd5b1c0fe971e15c483dc",
+    "url": "/website_v3.0/static/media/boba.d73ddec0.png"
+  },
+  {
+    "revision": "6dc77a4e0289db7b21edd9e7f1769ebe",
+    "url": "/website_v3.0/static/media/3d-candles.6dc77a4e.png"
+  },
+  {
+    "revision": "1b9eaab023c4b8abf1be3b101aa98caf",
+    "url": "/website_v3.0/static/media/fonts.1b9eaab0.png"
+  },
+  {
+    "revision": "c3a5b882eb6e9b6e32ec485fbf5f3ae5",
+    "url": "/website_v3.0/static/media/slope-day.c3a5b882.png"
+  },
+  {
+    "revision": "2f79c395764c1236fa56c63544be3a10",
+    "url": "/website_v3.0/static/media/take-care.2f79c395.png"
+  },
+  {
+    "revision": "cdf14761a3193d7b4be393af728c41ab",
+    "url": "/website_v3.0/static/media/sleep.cdf14761.png"
+  },
+  {
+    "revision": "716dd9ea689bce4b61e4810ff9ccba4c",
+    "url": "/website_v3.0/static/media/comics.716dd9ea.png"
+  },
+  {
+    "revision": "807a8eadc8b6868777380d603300631c",
+    "url": "/website_v3.0/static/media/chloe.807a8ead.png"
+  },
+  {
+    "revision": "4fe31e3cd9218a2ddcfefae1559d7541",
+    "url": "/website_v3.0/static/media/bujo.4fe31e3c.png"
+  },
+  {
+    "revision": "62832269984e40ef7edf4fe2ca05a834",
+    "url": "/website_v3.0/static/media/pw2.62832269.png"
+  },
+  {
+    "revision": "22bd7d307d3b64eff5e36de27316b166",
+    "url": "/website_v3.0/static/media/research.22bd7d30.png"
+  },
+  {
+    "revision": "bb29dd9260eca7f08d8841f2b2ebcd0e",
+    "url": "/website_v3.0/static/media/pw1.bb29dd92.png"
+  },
+  {
+    "revision": "d3cb36dca1aa2d8def6b358aa153d93d",
+    "url": "/website_v3.0/static/media/kwk.d3cb36dc.png"
+  },
+  {
+    "revision": "be9c727acdb69566c0cb992574932c40",
+    "url": "/website_v3.0/static/media/designs.be9c727a.png"
+  },
+  {
+    "revision": "c1591103da959e474b58b44b17280ee0",
+    "url": "/website_v3.0/static/media/PPEditorialNew-Ultralight.c1591103.otf"
+  },
+  {
+    "revision": "9f135dc1720dd11bcd8cd3c0e8d5c481",
+    "url": "/website_v3.0/index.html"
+  }
+];
