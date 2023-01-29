396de6aa0e0e176990be65e8590812a94f21a668
chloe <cac499@cornell.edu>
2023-01-29 09:57:14 -0500

    extra info

--- src/mobile/App.js
+++ src/mobile/App.js
@@ -30,6 +30,7 @@ function App() {
           <p class="main-text3">Hi! I’m Chloe, a first-year at <a class="link" href="https://www.cornell.edu/">Cornell University*</a> studying computer science and math. I’m driven to create a bridge between design and engineering to facilitate a better means of user experience and interaction with technology.</p>
           <p class="main-text3 no-margin">Feel free to connect with me on <a class="link" href="https://www.linkedin.com/in/chloeachann/">Linkedin*</a> or email me at <a class="link" href="mailto:cac499@gmail.com">cac499@cornell.edu*</a> to learn more!</p>
           <p class="text1 no-margin">* = click on text for more information</p>
+          <p class="text1 no-margin">**this website is best viewed via desktop!**</p>
           <p class="text1 bottom-left"><br></br><a onClick={() => scrollToSection(work)} className="link">↓ selected work<br></br></a></p>
           <br></br>
         </div>
